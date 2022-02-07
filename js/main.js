0f62848418457140f25e08cd68d70e029c57f501
Diego Arreceygor <darreceygor@gmail.com>
2022-02-07 17:04:14 -0300

    se agrego json para categorias, js y otros

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
-posicionarMenu();
- 
-$(window).scroll(function() {    
-    posicionarMenu();
-});
- 
-function posicionarMenu() {
-    var altura_del_header = $('.header').outerHeight(true);
-    var altura_del_menu = $('.menu').outerHeight(true);
- 
-    if ($(window).scrollTop() >= altura_del_header){
-        $('.menu').addClass('fixed');
-        $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
-    } else {
-        $('.menu').removeClass('fixed');
-        $('.wrapper').css('margin-top', '0');
-    }
+window.onload = traerCategories();
+
+async function traerCategories(){
+    await fetch('/json/categories.json') /*toma la direccion del arhivo json a tratar*/
+    .then(res => res.json()) /*traemos el archivo y lo transformamos a objeto json*/
+    .then(datosCategories =>{   /*traemos los datos para procesar*/
+        let cardGrid = document.getElementById("cardGrid");
+
+        for (let i in datosCategories) {         /*pongo el valor de cada campo*/
+            
+            cardGrid.innerHTML +=`
+                    <article class="gridItem">
+                        <div class="card">
+                            <div class="cardDetail">
+                                <h3>${datosCategories[i].category}</h3>
+                            </div>    
+                            
+                            <div class="iconCard">
+                                <a href="resources/categories.html"><img src="${datosCategories[i].image}" ></a>
+                            </div>
+                        </div>
+                    </article>
+                    `
+        }  
+    })
 }
- 
+
+
+
+
+
